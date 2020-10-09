54966aab9f3b1b97b68ba02b2c490b70e21d21c6
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-09 18:18:46 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,9 @@
 def find_max_value(array)
-  # Add your solution here
+  counter = 0 
+  new_array = array.sort.reverse()
+  max_value = -1 
+    while counter < array.length do 
+      if array[counter] == array.max
 end
+
+puts
